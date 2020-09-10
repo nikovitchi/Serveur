5307510dd1aae223c7bc4d485e2cdb0c062397d0
nikovitchi <43282050+nikovitchi@users.noreply.github.com>
2020-09-10 16:17:57 +0100

    added sound

--- static/interfaceScript.js
+++ static/interfaceScript.js
@@ -1,31 +1,33 @@
 
 $(function () {
-    var socket = io.connect();
-    const myName = prompt('Type your name here.');
+    var clientSocket = io.connect();
+    var anonymousCount = 1;
+    var myName = prompt('Type your name here.');
     var listItems = document.getElementById("messages").getElementsByTagName("li");
     var typing = false;
+    var title = document.title;
+    
 
-    //Call when the socket get created.
-    socket.emit('new user', myName);
+    //Call when the clientSocket get created.
+    clientSocket.emit('new user', myName);
 
     //Call when a 'new user' event is emitted to this client.
-    socket.on('new user', (name) => {
-        
-        $('#messages').append($('<li>').text(name + " logged in"));
-        listItems[listItems.length-1].style.background = "#4682B4";
-        $('#messages').stop().animate({ scrollTop: $("#messages")[0].scrollHeight }, 1000);
-        if(name == myName){
-            
+    clientSocket.on('new user', (name) => { 
+        if(clientSocket.socket.sessionid == myName){
+            $('#messages').append($('<li>').text("You logged in"));
             listItems[listItems.length-1].style.left = "0%";
         }else{
+            $('#messages').append($('<li>').text(name + " logged in"));
             listItems[listItems.length-1].style.left = "65%";
         }
+        listItems[listItems.length-1].style.background = "#4682B4";
+        $('#messages').stop().animate({ scrollTop: $("#messages")[0].scrollHeight }, 1000);
     });
 
     //Call when the textfield content is sent by pressing the button or enter.
     $('form').submit(function (e) {
         e.preventDefault(); // prevents page reloading
-        socket.emit('chat message', $('#textField').val());
+        clientSocket.emit('chat message', $('#textField').val());
         $('#textField').val('');
         return false;
     });
@@ -34,17 +36,17 @@ $(function () {
     $('#textField').on('keydown', function (name) {
         if (!typing) {
             typing = true;
-            socket.emit('userTyping');
+            clientSocket.emit('userTyping');
             var checkUserStoped = setInterval(function () {
                 typing = false;
-                socket.emit('userStopedTyping')
+                clientSocket.emit('userStopedTyping')
                 clearInterval(checkUserStoped);
             }, 5000)
         }
     });
 
-    //Call when another user on the same socket starts typing
-    socket.on('userTyping', function (usersTyping) {
+    //Call when another user on the same clientSocket starts typing
+    clientSocket.on('userTyping', function (usersTyping) {
         var usersTypingText = '';
         if (usersTyping.length == 0) {
             usersTypingText = usersTyping[0];
@@ -65,14 +67,15 @@ $(function () {
     })
 
     //Call when a 'chat message' event is received
-    socket.on('chat message', function (msg) {
-       
+    clientSocket.on('chat message', function (msg) {
+        
         if (msg.name == myName + ' : ') {
             $('#messages').append($('<li>').text("You : " + msg.msg));
             //listItems[listItems.length-1].innerHTML = "hi";
         } else {
             $('#messages').append($('<li>').text(msg.name + msg.msg));
             //listItems[listItems.length-1].innerHTML = "hi";
+            playSound("/static/notify.mp3");
         }
         listItems[listItems.length-1].style.background = msg.color;
         $('#messages').stop().animate({ scrollTop: $("#messages")[0].scrollHeight }, 1000);
@@ -90,4 +93,33 @@ $(function () {
 
 
 
+function playSound(url) {
+    const audio = new Audio(url);
+    audio.volume = 0.2;
+    audio.play();
+  }
 
+
+  function checkNamesForSpaces(name) {
+    var i = 0;
+
+    if (name.charAt(i) == '') {
+        name = 'Anonymous ' + anonymousCount
+        anonymousCount++;
+    }
+
+    do {
+        if (name.charAt(i) == ' ') {
+            i++;
+        } else {
+            break;
+        }
+        if (i == name.length) {
+            name = 'Anonymous ' + anonymousCount
+            anonymousCount++;
+        }
+    }
+    while (i < name.length);
+
+    return name;
+}
